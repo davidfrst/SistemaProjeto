4a49e7b32e22557542fd54b324f24eb06a6f1567
David <stdavst@gmail.com>
2026-09-04 10:09:55 -0300

    Add files via upload

--- aula1/src/Main.java
+++ aula1/src/Main.java
@@ -1,1 +1,14 @@
+public class Main{
 
+    public static void main(String[] args){
+
+        System.out.println("=================================");
+        System.out.println("       Sistema de Projetos");
+        System.out.println("=================================");
+        System.out.println("Projeto: Portfólio Acadêmico");
+        System.out.println("Desenvolvido em Java");
+        System.out.println("Versão 1.0");
+        
+
+    }
+}
